bff772ef61a7d300c0ad89124dd28eabdca3a8f1
sidharta <jeansidharta@gmail.com>
2024-10-11 05:08:57 -0400

    snipe now ignores current buffer

--- config/lua/plugins/snipe.lua
+++ config/lua/plugins/snipe.lua
@@ -1,21 +1,33 @@
 local snipe = require("snipe")
 snipe.setup()
 
--- These will receive a value at the end of this file
-local open_menu = nil
-local delete_menu = nil
+local function buffer_producer_filter_buf(buf)
+	return function()
+		local bufnr, bufnames = snipe.buffer_producer()
+		for index, iter_buf in ipairs(bufnr) do
+			if iter_buf == buf then
+				table.remove(bufnr, index)
+				table.remove(bufnames, index)
+				break
+			end
+		end
+		return bufnr, bufnames
+	end
+end
 
 local function create_open_menu()
-	return snipe.create_menu_toggler(snipe.buffer_producer, function(bufnr, _)
+	local cur_buf = vim.api.nvim_get_current_buf()
+	return snipe.create_menu_toggler(buffer_producer_filter_buf(cur_buf), function(bufnr, _)
 		vim.api.nvim_set_current_buf(bufnr)
 	end, { kind = "open" })
 end
 
 local function create_delete_menu()
-	return snipe.create_menu_toggler(snipe.buffer_producer, function(bufnr, _)
+	local cur_buf = vim.api.nvim_get_current_buf()
+	return snipe.create_menu_toggler(buffer_producer_filter_buf(cur_buf), function(bufnr, _)
 		vim.api.nvim_buf_delete(bufnr, {})
 		-- Reopen the delete menu so the user can chain delete
-		delete_menu()
+		create_delete_menu()()
 	end, { kind = "delete" })
 end
 
@@ -30,7 +42,7 @@ local function handle_autocmd_open_menu(bufnr, closeMenu)
 	set_current_win_title({ { "Snipe ", "FloatTitle" }, { "[Open]", "@text.emphasis" } })
 	vim.keymap.set("n", "<C-d>", function()
 		closeMenu()
-		delete_menu()
+		create_delete_menu()()
 	end, { buffer = bufnr })
 end
 
@@ -38,7 +50,7 @@ local function handle_autocmd_delete_menu(bufnr, closeMenu)
 	set_current_win_title({ { "Snipe ", "FloatTitle" }, { "[Delete]", "@text.danger" } })
 	vim.keymap.set("n", "<C-o>", function()
 		closeMenu()
-		open_menu()
+		create_open_menu()()
 	end, { buffer = bufnr })
 end
 
@@ -56,6 +68,6 @@ vim.api.nvim_create_autocmd("User", {
 	end,
 })
 
-open_menu = create_open_menu()
-delete_menu = create_delete_menu()
-vim.keymap.set("n", "gb", open_menu)
+vim.keymap.set("n", "gb", function()
+	create_open_menu()()
+end)
