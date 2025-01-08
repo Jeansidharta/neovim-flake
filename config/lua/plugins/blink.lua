24b6edda84fcebdb51f8efcdff8061dcf47a11b2
sidharta <jeansidharta@gmail.com>
2025-01-08 20:43:54 -0300

    chore: update inputs

--- config/lua/plugins/blink.lua
+++ config/lua/plugins/blink.lua
@@ -25,20 +25,9 @@ require("blink.cmp").setup({
 		},
 	},
 	snippets = {
-		expand = function(snippet)
-			require("luasnip").lsp_expand(snippet)
-		end,
-		active = function(filter)
-			if filter and filter.direction then
-				return require("luasnip").jumpable(filter.direction)
-			end
-			return require("luasnip").in_snippet()
-		end,
-		jump = function(direction)
-			require("luasnip").jump(direction)
-		end,
+		preset = "luasnip",
 	},
 	sources = {
-		default = { "lsp", "path", "luasnip", "buffer", "luasnip" },
+		default = { "lsp", "path", "snippets", "buffer" },
 	},
 })
