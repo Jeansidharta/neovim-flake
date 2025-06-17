c4197ba9a8086f82743455cd07c1444ee4b097dd
sidharta <jeansidharta@gmail.com>
2025-06-17 03:34:07 -0400

    Added nix to mdeval

--- config/lua/plugins/mdeval.lua
+++ config/lua/plugins/mdeval.lua
@@ -25,6 +25,14 @@ mdeval.setup({
 			exec_type = "argument",
 			language_code = "curl",
 		},
+		nix = {
+			-- Expose nixpkgs as pkgs
+			default_header = "rec { pkgs = (import <nixpkgs> {}); out = ",
+			default_footer = ";}",
+			command = { "nix", "eval", "--impure", "out", "--file" },
+			exec_type = "interpreted",
+			language_code = "nix",
+		},
 		usql = {
 			command = { "usql", "$$", "-q", "-f" },
 			exec_type = "interpreted",
