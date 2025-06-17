8b5994303abd50ba8902c023a91183de29893dc7
sidharta <jeansidharta@gmail.com>
2025-06-17 05:19:09 -0400

    Move to new way of registering lsps

--- config/lua/plugins/lsp-config.lua
+++ config/lua/plugins/lsp-config.lua
@@ -16,28 +16,70 @@ vim.api.nvim_create_autocmd("LspAttach", {
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr) end
 
-local capabilities = require("blink.cmp").get_lsp_capabilities()
+vim.lsp.enable("ts_ls")
+vim.lsp.config("lua_ls", {
+	on_init = function(client)
+		if client.workspace_folders then
+			local path = client.workspace_folders[1].name
+			if
+				path ~= vim.fn.stdpath("config")
+				and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+			then
+				return
+			end
+		end
 
-require("lspconfig").ts_ls.setup({ capabilities = capabilities })
-require("lspconfig").lua_ls.setup({ capabilities = capabilities })
-require("lspconfig").bashls.setup({ capabilities = capabilities })
-require("lspconfig").cssls.setup({ capabilities = capabilities })
-require("lspconfig").eslint.setup({ capabilities = capabilities })
-require("lspconfig").gleam.setup({ capabilities = capabilities })
-require("lspconfig").html.setup({ capabilities = capabilities })
-require("lspconfig").nginx_language_server.setup({ capabilities = capabilities })
-require("lspconfig").sqls.setup({ capabilities = capabilities })
-require("lspconfig").jsonls.setup({ capabilities = capabilities })
-require("lspconfig").marksman.setup({ capabilities = capabilities })
-require("lspconfig").svelte.setup({ capabilities = capabilities })
-require("lspconfig").terraformls.setup({ capabilities = capabilities })
-require("lspconfig").zk.setup({ capabilities = capabilities })
-require("lspconfig").nil_ls.setup({ capabilities = capabilities })
-require("lspconfig").rust_analyzer.setup({ capabilities = capabilities })
-require("lspconfig").emmet_language_server.setup({})
-require("lspconfig").astro.setup({ capabilities = capabilities })
-require("lspconfig").zls.setup({
-	capabilities = capabilities,
+		client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most
+				-- likely LuaJIT in the case of Neovim)
+				version = "LuaJIT",
+				-- Tell the language server how to find Lua modules same way as Neovim
+				-- (see `:h lua-module-load`)
+				path = {
+					"lua/?.lua",
+					"lua/?/init.lua",
+				},
+			},
+			-- Make the server aware of Neovim runtime files
+			workspace = {
+				checkThirdParty = false,
+				library = {
+					vim.env.VIMRUNTIME,
+				},
+				-- Or pull in all of 'runtimepath'.
+				-- NOTE: this is a lot slower and will cause issues when working on
+				-- your own configuration.
+				-- See https://github.com/neovim/nvim-lspconfig/issues/3189
+				-- library = {
+				--   vim.api.nvim_get_runtime_file('', true),
+				-- }
+			},
+		})
+	end,
+	settings = {
+		Lua = {},
+	},
+})
+vim.lsp.enable("lua_ls")
+vim.lsp.enable("bashls")
+vim.lsp.enable("cssls")
+vim.lsp.enable("eslint")
+vim.lsp.enable("gleam")
+vim.lsp.enable("html")
+vim.lsp.enable("nginx_language_server")
+vim.lsp.enable("sqls")
+vim.lsp.enable("jsonls")
+vim.lsp.enable("marksman")
+vim.lsp.enable("svelte")
+vim.lsp.enable("terraformls")
+vim.lsp.enable("zk")
+vim.lsp.enable("nil_ls")
+vim.lsp.enable("rust_analyzer")
+vim.lsp.enable("emmet_language_server")
+vim.lsp.enable("astro")
+vim.lsp.enable("zls")
+vim.lsp.config("zls", {
 	settings = {
 		zls = {
 			enable_build_on_save = true,
@@ -46,8 +88,8 @@ require("lspconfig").zls.setup({
 	},
 })
 
-require("lspconfig").openscad_ls.setup({
-	capabilities = capabilities,
+vim.lsp.enable("openscad_ls")
+vim.lsp.config("openscad_ls", {
 	cmd = {
 		"openscad-lsp",
 		"--stdio",
