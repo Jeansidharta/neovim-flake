2345561e8991a4fa574e2dfa2c4f1109e51d3684
sidharta <jeansidharta@gmail.com>
2025-09-26 22:32:47 -0400

    Update literate-markdown

--- flake.nix
+++ flake.nix
@@ -10,6 +10,7 @@
     # Literate programming builder
     literate-markdown = {
       url = "github:jeansidharta/literate-markdown";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     # Libraries
